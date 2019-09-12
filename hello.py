b68ae8995754aabc19d78081b41626d564fcdab6
Jesper Petersen <info@ordklar.dk>
2019-09-12 10:19:49 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -28,9 +28,15 @@ hire_crew('Mads', 'Jesper', 'Sofie', 'Jonas')
 
 #Lambda funtion
 #1
+print("First Lambda")
 print((lambda x: x + 1)(2))
 
 #2
+print("two")
 add_one = lambda x: x + 2
 print(add_one(4))
 
+print("three")
+high_ord_func = lambda x, func: x + func(x)
+x = high_ord_func(5, lambda x: x*x)
+print(x)
