f186512c6539b369f1466164d70fc7159d06d8c4
Jesper Petersen <info@ordklar.dk>
2019-09-15 13:38:58 +0200

    update

--- hello_2.py
+++ hello_2.py
@@ -1,6 +1,18 @@
 x = "Jesper"
 
 def myfun():
+    x = "Kasper"
     print("Hej med dig, " + x)
 
 myfun()
+
+print("Hej " + x)
+
+x = "Jonas"
+def myfun2():
+    global x
+    x = "Google"
+
+myfun2()
+
+print("Hej " + x)
