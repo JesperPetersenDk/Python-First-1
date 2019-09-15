cbef5f44555235eaa5b167f6c89efe6c5d368f2b
Jesper Petersen <info@ordklar.dk>
2019-09-15 13:18:00 +0200

    update

---
+++ test.py
@@ -0,0 +1,20 @@
+x = 5
+y = 4
+print(y)
+print(x)
+
+
+r = "Jesper"
+print(r)
+
+k,q,a = "hello world", "test", "Google"
+print(k)
+print(q)
+print(a)
+
+s = "Awesome"
+print("python er " + s)
+
+
+b = s + a + r
+print(b)
